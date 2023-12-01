0e1806fbb4202f7f1e0c78ae5cd3c3944e6961af
Kaius <kaius.loos@gmail.com>
2023-12-01 19:39:48 +0200

    add simple cli to run solutions of specific day

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 use aoc2023::day1;
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    /// Run only the solution for specified day. If not specified the solutions for all days are run.
+    #[arg(short, long, value_parser = clap::value_parser!(u8).range(1..26))]
+    day: Option<u8>,
+}
 
 fn main() {
-    day1::run()
+    let cli = Cli::parse();
+    match cli.day {
+        Some(1) => day1::run(),
+        None => {
+            day1::run();
+        }
+        Some(d) => println!("day {d} not implemented"),
+    }
 }
